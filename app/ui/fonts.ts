e165e838e59f380be4e64f456b196832c1ea737e
Michelly Nonato <smartnovo01@gmail.com>
2023-12-14 13:00:46 -0300

    Styled font for dashboard

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,4 +4,4 @@ import { Lusitana } from 'next/font/google';
  
 export const poppins = Poppins({weight: ['300', '600', '800'], subsets: ['latin'] });
 export const inter = Inter({subsets: ['latin']});
-export const lusitana = Lusitana({weight: ['400'], subsets: ['latin']});
+export const lusitana = Poppins({weight: ['400'], subsets: ['latin']});
